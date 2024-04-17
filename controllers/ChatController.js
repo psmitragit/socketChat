e65d1732c10cb1c689d83d00e84ce1e0021e70a2
Ayan <karmakarayan217@gmail.com>
2024-04-17 13:27:29 +0530

    chat saving to db

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -1,5 +1,5 @@
 // controllers/chatController.js
-
+const ChatMessage = require('../models/ChatMessage.js');
 class ChatController {
   // Render the chat view
   showChatPage(req, res) {
@@ -9,6 +9,12 @@ class ChatController {
   // Handle chat message
   handleChatMessage(io) {
     return async (msg) => {
+      // Extract message data
+      const { username, message } = msg;
+      // Save the message to the database
+      const newMessage = new ChatMessage({ username, message });
+      await newMessage.save();
+
       try {
         io.emit('chat message', msg); // Broadcast the message to all connected clients
       } catch (err) {
---
+++ models/ChatMessage.js
@@ -0,0 +1,10 @@
+// models/ChatMessage.js
+const mongoose = require('mongoose');
+
+const chatMessageSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('ChatMessage', chatMessageSchema);
